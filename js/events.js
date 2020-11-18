dc449db621f073137d59c6a9ea4d2bce061b1c0a
glorious-communicator-0033 <clinton.weaver@gmail.com>
2020-11-18 02:59:24 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -11,6 +11,20 @@ function frameIt() {
   });
 }
 
+function submitIt() {
+  $('form').on('submit', function(){
+    alert("Your form is going to be submitted now.");
+  });
+}
+
+function pressIt() {
+  $('form input').on('keydown', function(key){
+    if(key.which == 71){
+      alert("G was pressed");
+    }
+  });
+}
+
 $(document).ready(function(){
 
 // call functions here
